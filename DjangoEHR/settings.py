6075da0a33a3318df6fa0afb255abb96d02caf5e
Mostafa Khaled <mostafadarwish9392@gmail.com>
2024-03-10 17:22:36 +0200

    data migration

--- DjangoEHR/settings.py
+++ DjangoEHR/settings.py
@@ -90,8 +90,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
-        'USER': 'lab3database',
-        'PASSWORD': 'Mypassword3',
+        'USER': 'mostafalab1',
+        'PASSWORD': 'Mostafa93_00',
         'HOST': 'ehrserver.postgres.database.azure.com',
         'PORT': '5432',
         'OPTIONS':{'sslmode':'require'},
